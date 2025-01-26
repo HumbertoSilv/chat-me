04dd293e01ab5b71332520dd05c190e07a440f5e
Humberto <matheushumberto520@gmail.com>
2025-01-26 19:57:21 -0400

    style(global): Will make small adjustments to the color and screen size stylization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import '@/app/ui/globals.css';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased text-gray-400`}
       >
         {children}
       </body>
