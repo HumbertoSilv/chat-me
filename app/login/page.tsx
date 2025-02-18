e2c86ea702d79a61057af2b83dd5c9369ec3d216
Humberto <matheushumberto520@gmail.com>
2025-02-17 21:38:09 -0400

    feat(login): will create login page

---
+++ app/login/page.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import * as Input from "@/app/ui/Input";
+import Link from "next/link";
+import { useActionState } from "react";
+import { actionLogin } from "../api/login/action";
+
+export default function Login() {
+  const [, action, isPending] = useActionState(actionLogin, null)
+
+  return (
+    <div className="p-8">
+      <form
+        action={action}
+        className="flex flex-col max-w-[450px] m-auto mt-18">
+        <h1 className="text-6xl font-medium py-8 text-white mb-8">
+          Faça seu login.
+        </h1>
+
+        <label
+          htmlFor="username"
+          className="flex flex-col font-normal text-gray-200 mb-4"
+        >
+          Username
+          <Input.Root className="bg-zinc-800 rounded-full py-1">
+            <Input.Control
+              id="username"
+              name="username"
+              type="username"
+              placeholder="Sua senha..."
+              required
+            />
+          </Input.Root>
+          <Input.Error />
+        </label>
+
+        <label
+          htmlFor="email"
+          className="flex flex-col font-normal text-gray-200"
+        >
+          E-mail
+          <Input.Root className="bg-zinc-800 rounded-full py-1">
+            <Input.Control
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Seu e-mail..."
+              required
+            />
+          </Input.Root>
+          <Input.Error />
+        </label>
+
+        <span className="text-sm text-gray-200 self-end" >
+          <Link href="/recover-password">
+            <u>Esqueci minha senha</u>
+          </Link>
+        </span>
+
+        <button
+          disabled={isPending}
+          type="submit"
+          className="bg-zinc-700 p-3 mt-5 w-5/6 self-center rounded-full text-sm text-gray-200 font-semibold hover:bg-zinc-800 transition disabled:bg-zinc-800"
+        >
+          Entrar
+        </button>
+
+        <button
+          className="flex gap-2 bg-white text-zinc-800 text-sm p-1 mt-4 w-2/3 self-center justify-center items-center rounded-2xl"
+        >
+          Faça login com o Google
+        </button>
+
+        <span className="self-center my-5" >
+          <Link href="/sign-up">Ainda não tem uma conta?</Link>
+        </span>
+      </form>
+    </div>
+  )
+}
