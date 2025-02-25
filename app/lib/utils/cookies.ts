61baff50586522eb586d7d85c44de813a4ab712f
Humberto <matheushumberto520@gmail.com>
2025-02-24 21:02:19 -0400

    feat(cookies): add utility function to retrieve access token from cookies

---
+++ app/lib/utils/cookies.ts
@@ -0,0 +1,8 @@
+import { cookies } from "next/headers"
+
+export const getAccessToken = async (): Promise<string> => {
+  const cookieStore = await cookies()
+  const accessToken = cookieStore.get('access_token')
+
+  return accessToken?.value as unknown as string
+}
