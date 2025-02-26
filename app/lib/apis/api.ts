8c5f09209ec68adecd37b882832ea7540297d2f1
Humberto <matheushumberto520@gmail.com>
2025-02-25 21:32:36 -0400

    feat(api): add user profile and direct chats request functions with authentication

--- app/lib/apis/api.ts
+++ app/lib/apis/api.ts
@@ -6,6 +6,27 @@ interface ILogin {
   password: string
 }
 
+interface IChatsResponse {
+  id: string
+  chat_type: string
+  users: [
+    {
+      username: string
+      email: string
+      name?: string
+      avatar_url?: string
+    }
+  ],
+  messages: []
+}
+
+interface ProfileResponse {
+  username: string
+  email: string
+  name?: string
+  avatar_url?: string
+}
+
 
 export async function login({ username, password }: ILogin): Promise<void> {
   const formData = new URLSearchParams()
@@ -34,4 +55,43 @@ export async function login({ username, password }: ILogin): Promise<void> {
     sameSite: 'none',
     secure: true
   })
+}
+
+export async function getUserProfileReq(): Promise<ProfileResponse> {
+  const cookieStore = await cookies()
+  const accessToken = cookieStore.get('access_token')
+
+  const response = await fetch(
+    'http://localhost:8000/users/profile', // TODO: set URL in ENV
+    {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${accessToken?.value}`,
+        'Accept': 'application/json',
+      },
+    }
+  )
+
+  const userInfos = await response.json()
+
+  return userInfos
+}
+
+export async function getDirectChatsReq(): Promise<IChatsResponse[]> {
+  const cookieStore = await cookies()
+  const accessToken = cookieStore.get('access_token')
+
+  const response = await fetch(
+    'http://localhost:8000/chats/direct',
+    {
+      headers: {
+        'Authorization': `Bearer ${accessToken?.value}`,
+        'Accept': 'application/json',
+      },
+      // credentials: 'same-origin',
+    })
+
+  const chats = await response.json()
+
+  return chats
 }
