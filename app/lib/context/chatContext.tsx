8cd0ed08ae29f445917141ab71ac23a1aafc80a8
Humberto <matheushumberto520@gmail.com>
2025-03-20 20:09:37 -0400

    feat(chat): enhance chat context to manage current chat state and update user structure

--- app/lib/context/chatContext.tsx
+++ app/lib/context/chatContext.tsx
@@ -1,33 +1,44 @@
 'use client';
 
 import React, { createContext, useContext, useState } from 'react';
+import { IUserPublic } from './userContext';
+
+interface IMessage {
+  id: string
+  content: string
+  createdAt: string
+  updatedAt?: string
+}
 
 export type IChat = {
   chatId: string
   chatType: string
-  users: [
-    {
-      username: string
-      email: string
-      name?: string
-      avatar_url?: string
-    }
-  ]
+  users: IUserPublic[]
+  lastMessage: IMessage
 }
+
+export type ICurrentChat = Omit<IChat, 'lastMessage'>
 
 type ChatsContextType = {
   chats: IChat[]
   setChats: (data: IChat[]) => void;
+  currentChat: ICurrentChat | null;
   getChatByUsername: (username: string) => IChat | undefined;
+  setCurrentChat: (chat: ICurrentChat) => void;
 }
 
 const ChatsContext = createContext<ChatsContextType>({} as ChatsContextType);
 
 export const ChatsProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [chats, setChatsState] = useState<IChat[]>([])
+  const [currentChat, setCurrentChatState] = useState<ICurrentChat | null>(null)
 
   const setChats = (data: IChat[]) => {
     setChatsState(data)
+  }
+
+  const setCurrentChat = (chat: ICurrentChat) => {
+    setCurrentChatState(chat)
   }
 
   const getChatByUsername = (username: string) => {
@@ -35,7 +46,7 @@ export const ChatsProvider: React.FC<{ children: React.ReactNode }> = ({ childre
   }
 
   return (
-    <ChatsContext.Provider value={{ chats, setChats, getChatByUsername }}>
+    <ChatsContext.Provider value={{ chats, setChats, getChatByUsername, currentChat, setCurrentChat }}>
       {children}
     </ChatsContext.Provider>
   );
